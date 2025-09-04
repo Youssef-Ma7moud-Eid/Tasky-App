f91fb64d8d971dba77f8ffcef6a6310a7687e8f5
Youssef-Mahmoud-Eid <youssef.mahmoud51098@gmail.com>
2025-09-04 22:06:26 +0300

    first commit

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  static const String iconsCloseIcon = "assets/icons/close-icon.png";
+
+  static const String iconsDeleteIcon = "assets/icons/delete-icon.png";
+
+  static const String iconsEditIcon = "assets/icons/edit-icon.png";
+
+  static const String iconsLogoutIcon = "assets/icons/logout-icon.png";
+
+  static const String iconsPriority = "assets/icons/priority.png";
+
+  static const String iconsSearchIcon = "assets/icons/search-icon.png";
+
+  static const String iconsSend = "assets/icons/send.png";
+
+  static const String iconsTimer = "assets/icons/timer.png";
+
+  static const String imagesEmptyTask = "assets/images/empty_task.png";
+
+  static const String imagesLogo = "assets/images/logo.png";
+
+  static const String imagesOnboarding1 = "assets/images/onboarding1.png";
+
+  static const String imagesOnboarding2 = "assets/images/onboarding2.png";
+
+  static const String imagesOnboarding3 = "assets/images/onboarding3.png";
+
+  static const String imagesPc = "assets/images/pc.lnk";
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TaskyApp());
+}
+
+class TaskyApp extends StatelessWidget {
+  const TaskyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
