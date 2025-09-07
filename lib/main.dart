fcc52981c115e29079a51cbfc6e9eea8509badec
Youssef-Mahmoud-Eid <youssef.mahmoud51098@gmail.com>
2025-09-07 05:03:17 +0300

    add firebase with custon splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:tasky/features/add-task/views/tasks_view.dart';
+import 'package:animate_do/animate_do.dart';
+import 'package:tasky/core/utils/app_colors.dart';
+import 'package:tasky/features/onboarding/views/onboarding_view.dart';
+import 'package:tasky/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const TaskyApp());
 }
 
@@ -11,6 +17,72 @@ class TaskyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: TasksView());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const CustomSplashScreen(),
+    );
+  }
+}
+
+class CustomSplashScreen extends StatelessWidget {
+  const CustomSplashScreen({super.key});
+
+  @override
+  @override
+  Widget build(BuildContext context) {
+    const text = "Tasky";
+
+    return Scaffold(
+      backgroundColor: AppColors.primaryColor,
+      body: Center(
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: List.generate(text.length, (index) {
+            final letter = text[index];
+            final isLast = index == text.length - 1;
+
+            return index.isEven
+                ? FadeOutUp(
+                    onFinish: (data) {
+                      if (isLast) {
+                        Navigator.pushReplacement(
+                          context,
+                          PageRouteBuilder(
+                            pageBuilder:
+                                (context, animation, secondaryAnimation) {
+                                  return OnboardingView();
+                                },
+                          ),
+                        );
+                      }
+                    },
+                    delay: Duration(milliseconds: 400 * index),
+                    duration: const Duration(milliseconds: 600),
+                    child: Text(
+                      letter,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        fontSize: 55,
+                        color: isLast ? const Color(0xffF5F876) : Colors.white,
+                      ),
+                    ),
+                  )
+                : FadeOutDown(
+                    delay: Duration(milliseconds: 400 * index),
+                    duration: const Duration(milliseconds: 600),
+
+                    child: Text(
+                      letter,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        fontSize: 55,
+                        color: isLast ? const Color(0xffF5F876) : Colors.white,
+                      ),
+                    ),
+                  );
+          }),
+        ),
+      ),
+    );
   }
 }
