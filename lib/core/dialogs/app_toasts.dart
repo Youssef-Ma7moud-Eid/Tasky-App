e83be2a8c53bb95fab2a004e10f9ac1e342b96be
Youssef-Mahmoud-Eid <youssef.mahmoud51098@gmail.com>
2025-09-30 01:11:42 +0300

    add toast

--- lib/core/dialogs/app_toasts.dart
+++ lib/core/dialogs/app_toasts.dart
@@ -48,3 +48,50 @@ abstract class AppToast {
     );
   }
 }
+abstract class Apptot {
+  static void showToast({
+    required BuildContext context,
+    required String title,
+    required String description,
+    required ToastificationType type,
+  }) {
+    toastification.show(
+      context: context,
+      type: type,
+      title: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      description: Text(
+        description,
+        style: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+        ),
+      ),
+      primaryColor: Colors.white,
+      autoCloseDuration: const Duration(seconds: 3),
+      progressBarTheme: ProgressIndicatorThemeData(
+        color: type == ToastificationType.success
+            ? Colors.green
+            : type == ToastificationType.info
+                ? Colors.blue
+                : type == ToastificationType.warning
+                    ? Colors.orange
+                    : Colors.red,
+      ),
+      showProgressBar: true,
+      backgroundColor: type == ToastificationType.success
+          ? Colors.green
+          : type == ToastificationType.info
+              ? Colors.blue
+              : type == ToastificationType.warning
+                  ? Colors.orange
+                  : Colors.red,
+      foregroundColor: Colors.white,
+    );
+  }
+}
